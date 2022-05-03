3de0cb05486c9d9ecf7382eda6f2944b54d630ab
Thanh-Quy Nguyen <nguyen.tq@hotmail.fr>
2022-05-03 16:26:40 +0200

    :wrench: Configure project

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: false,
+    es2021: true,
+    mocha: true,
+    node: true,
+  },
+  extends: [
+    "standard",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:node/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  overrides: [
+    {
+      files: ["hardhat.config.js"],
+      globals: { task: true },
+    },
+  ],
+};
---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+  proseWrap: "never",
+  htmlWhitespaceSensitivity: "strict",
+  endOfLine: "lf",
+};
